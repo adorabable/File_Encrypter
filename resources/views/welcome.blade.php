56e771b9e31d3d096584f9eaf7081388b6c6546d
rividya <ivy.aluna@gmail.com>
2019-12-09 19:06:28 +0700

    tampilan utama

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,13 +1,22 @@
 <html>
 <head>
-	<title>Tutorial Laravel #30 : Membuat Upload File Dengan Laravel</title>
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
- 
+	<title>eNCRypt</title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
+
+<link href="CSS.css" rel="stylesheet">
+
 <body>
+	<img class="logo" src="logo.png" alt="logo">
+
 	<div class="row">
 		<div class="container">
-			<h2 class="text-center my-5">Tutorial Laravel #30 : Membuat Upload File Dengan Laravel</h2>
+			<h2 class="title">Online Encrypt Converter 
+			</h2>
+			<p class="text" >Easily encrypt your documents in seconds</p>
+			
 			
 			<div class="col-lg-8 mx-auto my-5">	
  
@@ -18,22 +27,28 @@
 					@endforeach
 				</div>
 				@endif
- 
-				<form action="/upload/proses" method="POST" enctype="multipart/form-data">
+				<div class="box1"></div>
+					<div class="form-group box2 file"></div>
+					<div class="upload-files">Upload Files</div>
+
+				<!-- <form action="/upload/proses" method="POST" enctype="multipart/form-data">
 					{{ csrf_field() }}
  
+					
+					<div class="drop-files">drop files here</div> 
+
 					<div class="form-group">
 						<b>File Gambar</b><br/>
 						<input type="file" name="file">
 					</div>
- 
 					<div class="form-group">
 						<b>Keterangan</b>
 						<textarea class="form-control" name="keterangan"></textarea>
 					</div>
  
-					<input type="submit" value="Upload" class="btn btn-primary">
-				</form>
+				<input type="submit" value="Upload" class="btn btn-primary"> 
+				</form> -->
+				<div class="copy-right"></div>
 			</div>
 		</div>
 	</div>
